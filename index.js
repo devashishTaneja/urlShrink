3c145b109aa023fd650c7932d31e61c695e1ea4b
Devashish Taneja <dtanejaxyz@gmail.com>
2021-07-01 18:06:24 +0530

    Added support for dynamic port heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
     // middleware static
     app.use(express.static('public'));
     app.set('view engine','ejs');
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 
     app.get('/', function(req, res){
         res.render('home');
