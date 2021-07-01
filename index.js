790bbd87f705f9947d67be86771f7eca181d3ea5
Devashish Taneja <dtanejaxyz@gmail.com>
2021-07-01 18:27:06 +0530

    Changing path of views

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     // middleware static
     app.use(express.static('public'));
     app.set('view engine','ejs');
-    app.set('views', path.join(__dirname, '/views'));
+    app.set('views', path.join(__dirname, '/Views'));
     app.listen(process.env.PORT || 3000);
 
     app.get('/', function(req, res){
